6b3a3ed28d1d85af01be8f64cb435e8d5bb90af1
lmcli38 <lmc199938@gmail.com>
2023-03-11 15:02:25 -0500

    test on eqaution

--- src/pages/equations.js
+++ src/pages/equations.js
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React from 'react';
+import MathJax from "react-mathjax";
 const equations = () => {
-    return (
-        <div>
-            <h1>equations</h1>
-        </div>
-    )
+    const InflationRATE = `(CPI_x+1 - CPI_x)/CPI-x`;
+    const GDP = `GDP=C+G+I+NX`;
+    return <div style={{padding: 50}}>
+        <MathJax.Provider>
+            <div>
+                <p>
+                    Inflation rate:<MathJax.Node inline formula={InflationRATE} />
+                </p>
+                <p>
+                    GDP:<MathJax.Node inline formula={GDP}/>
+                </p>
+            </div>
+        </MathJax.Provider>
+    </div>
 }
 
 export default equations
